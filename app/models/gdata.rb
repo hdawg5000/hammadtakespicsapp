417323d05676f21dc2a16485e76523a436c6333d
Hammad Naseem <hammadnsm@gmail.com>
2017-03-25 23:28:07 +0000

    comment out gdata.rb

--- app/models/gdata.rb
+++ app/models/gdata.rb
@@ -4,8 +4,8 @@ require 'base64'
 
 uri = 'https://picasaweb.google.com/data/feed/api/user/105408009953777071792/albumid/6245274247532227233'
 
-Net::HTTP.start(uri.host, uri.port) do |http|
-  esc = URI.escape(uri)
-  uri = URI.parse(esc)
-  puts Net::HTTP:get(uri)
-end
+# Net::HTTP.start(uri.host, uri.port) do |http|
+#   esc = URI.escape(uri)
+#   uri = URI.parse(esc)
+#   puts Net::HTTP:get(uri)
+# end
