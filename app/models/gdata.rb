fae4ec6e88c61e059ed1338562a876e9e014bcee
Hammad Naseem <hammadnsm@gmail.com>
2017-03-25 23:21:52 +0000

    Styling updates

---
+++ app/models/gdata.rb
@@ -0,0 +1,11 @@
+require 'net/https'
+require 'uri'
+require 'base64'
+
+uri = 'https://picasaweb.google.com/data/feed/api/user/105408009953777071792/albumid/6245274247532227233'
+
+Net::HTTP.start(uri.host, uri.port) do |http|
+  esc = URI.escape(uri)
+  uri = URI.parse(esc)
+  puts Net::HTTP:get(uri)
+end
