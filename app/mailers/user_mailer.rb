65942874dde12108d725e9b5e435017ccef888e5
Hammad Naseem <hammadnsm@gmail.com>
2017-04-25 01:08:23 +0000

    Updates to contact form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,16 @@
 class UserMailer < ApplicationMailer
   default from: 'saycheese@hammadtakespics.com'
  
-  def contact_form_email(name, email, subject)
+  def contact_form_email(name, email, subject, *body)
     @name = name
     @email = email
     @subject = subject
-    mail(to: @email, subject: "Hammad Takes Pics: Message Received", content_type: "text/html")
-    #mail(to: "saycheese@hammadtakespics.com", subject: "Message Received", content_type: "text/html")
+    @body = body
+    if (body == null)
+      mail(to: @email, subject: "Hammad Takes Pics: Message Received", content_type: "text/html")
+    else
+      mail(to: "saycheese@hammadtakespics.com", subject: "Message Received From #{@name}", content_type: "text/html",
+        body: @body)
+    end
   end
 end
