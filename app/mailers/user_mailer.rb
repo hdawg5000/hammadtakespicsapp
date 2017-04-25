515a1322654765f4c498b54cf9773ffb4c6f676d
Hammad Naseem <hammadnsm@gmail.com>
2017-04-25 01:19:25 +0000

    Updates to contact form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,11 +6,11 @@ class UserMailer < ApplicationMailer
     @email = email
     @subject = subject
     @body = body
-    if @body.nil?
+    if @body.empty?
       mail(to: @email, subject: "Hammad Takes Pics: Message Received", content_type: "text/html")
     else
       mail(to: "saycheese@hammadtakespics.com", subject: "Message Received From #{@name}", content_type: "text/html",
-        body: @body)
+        body: "Subject: #{@subject} \n Body: #{@body}")
     end
   end
 end
