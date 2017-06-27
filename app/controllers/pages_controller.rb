9c8dfac6eed3ec16da29965352e056d5ac43caa0
Hammad Naseem <hammadnsm@gmail.com>
2017-06-27 19:21:00 +0000

    Updated pages controller to parse through XMl files and pass to views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,9 +18,21 @@ class PagesController < ApplicationController
   end
   
   def italy
+    @doc = Nokogiri::XML(File.open("app/assets/italy.xml"))
+    @entries = @doc.css("entry content")
+    @pic_urls = []
+    @entries.each do |x|
+      @pic_urls.push(x["src"])
+    end
   end
   
   def turkey
+    @doc = Nokogiri::XML(File.open("app/assets/turkey.xml"))
+    @entries = @doc.css("entry content")
+    @pic_urls = []
+    @entries.each do |x|
+      @pic_urls.push(x["src"])
+    end
   end
   
   def dubai
